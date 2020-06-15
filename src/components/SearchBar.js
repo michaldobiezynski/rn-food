c611013c993d69225cd43eb8622d27ea26d8937a
michaldobiezynski <dobiezynski@gmail.com>
2020-06-15 21:19:57 +0100

    Managing State and detecting Editing completion

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,21 @@ import React from "react";
 import { StyleSheet, Text, View, TextInput } from "react-native";
 import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
-      <TextInput style={styles.inputStyle} placeholder="Search" />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={term}
+        onChangeText={(newTerm) => onTermChange(newTerm)}
+        style={styles.inputStyle}
+        placeholder="Search"
+        onEndEditing={() => {
+          onTermSubmit();
+        }}
+      />
     </View>
   );
 };
