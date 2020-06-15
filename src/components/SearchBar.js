c1cf3ebcf65ee6794cb1f845a866b709c514b9c2
michaldobiezynski <dobiezynski@gmail.com>
2020-06-15 20:54:45 +0100

    dispalying icons

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
 const SearchBar = () => {
   return (
     <View style={styles.background}>
+      <Feather name="search" />
       <Text>Search</Text>
     </View>
   );
