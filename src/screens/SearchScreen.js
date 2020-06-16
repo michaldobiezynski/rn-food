bf546e57da8ac10f413f206f60bdd6fb8f564116
michaldobiezynski <dobiezynski@gmail.com>
2020-06-16 09:14:22 +0100

    running an initial search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: "san jose",
         },
       });
@@ -26,7 +26,13 @@ const SearchScreen = () => {
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => {
+          searchApi(term);
+        }}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
