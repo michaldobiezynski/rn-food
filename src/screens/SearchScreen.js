b269cefad0d88fcc0863b630b71419159715a7ce
michaldobiezynski <dobiezynski@gmail.com>
2020-06-17 08:42:52 +0100

    the UseEffect Hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import yelp from "../api/yelp";
@@ -23,6 +23,10 @@ const SearchScreen = () => {
       setErrorMessage("Something went wrong.");
     }
   };
+
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
 
   return (
     <View>
